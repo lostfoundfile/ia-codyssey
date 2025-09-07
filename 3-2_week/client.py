31d8f0534b35f336662d9c3ddcbd7511a97eb281
lostfoundfile <163130598+lostfoundfile@users.noreply.github.com>
2025-09-08 00:21:44 +0900

    Add files via upload

--- 3-2_week/client.py
+++ 3-2_week/client.py
@@ -1,33 +1,42 @@
 import socket
 import threading
 
-def receive_messages():
+def receive_messages(client_socket):
     while True:
         try:
-            message = client.recv(1024).decode('utf-8')
+            message = client_socket.recv(1024).decode('utf-8')
             if message:
                 print(message)
         except:
             print('서버와의 연결이 종료되었습니다.')
-            client.close()
+            client_socket.close()
             break
 
-def send_messages():
+def send_messages(client_socket):
     while True:
         message = input()
-        client.send(message.encode('utf-8'))
+        client_socket.send(message.encode('utf-8'))
         if message == '/종료':
             break
 
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 999
+
+    try:
+        client_socket.connect((host, port))
+        print(client_socket.recv(1024).decode('utf-8'))  # 닉네임 요청
+        nickname = input('닉네임 입력: ')
+        client_socket.send(nickname.encode('utf-8'))
+
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+        send_thread = threading.Thread(target=send_messages, args=(client_socket,))
+        receive_thread.start()
+        send_thread.start()
+
+    except ConnectionRefusedError:
+        print('서버에 연결할 수 없습니다. 서버가 실행 중인지 확인하세요.')
+
 if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = 55555
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
-
-    receive_thread = threading.Thread(target=receive_messages)
-    receive_thread.start()
-
-    send_thread = threading.Thread(target=send_messages)
-    send_thread.start()
+    start_client()
--- 3-2_week/server.py
+++ 3-2_week/server.py
@@ -4,9 +4,9 @@ import threading
 clients = []
 nicknames = []
 
-def broadcast(message, sender_socket=None):
+def broadcast(message, exclude_socket=None):
     for client in clients:
-        if client != sender_socket:
+        if client != exclude_socket:
             try:
                 client.send(message.encode('utf-8'))
             except:
@@ -16,45 +16,44 @@ def handle_client(client):
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            if message == '/종료':
+            if not message:
+                break
+
+            if message.startswith('/종료'):
                 index = clients.index(client)
                 nickname = nicknames[index]
                 leave_message = f'{nickname}님이 퇴장하셨습니다.'
-                print(leave_message)
-                broadcast(leave_message, client)
+                broadcast(leave_message)
                 client.close()
                 remove_client(client)
                 break
+
             elif message.startswith('/귓속말'):
                 parts = message.split(' ', 2)
                 if len(parts) < 3:
-                    client.send('귓속말 형식: /귓속말 닉네임 메시지'.encode('utf-8'))
+                    client.send('형식: /귓속말 닉네임 메시지'.encode('utf-8'))
                     continue
-                to_nickname = parts[1]
-                private_message = parts[2]
-                send_private_message(client, to_nickname, private_message)
+                to_nick = parts[1]
+                msg = parts[2]
+                send_private_message(client, to_nick, msg)
             else:
                 index = clients.index(client)
                 nickname = nicknames[index]
                 full_message = f'{nickname}> {message}'
-                print(full_message)
                 broadcast(full_message, client)
         except:
             remove_client(client)
             break
 
-def send_private_message(sender, to_nickname, message):
-    if to_nickname in nicknames:
-        index = nicknames.index(to_nickname)
-        recipient = clients[index]
-        sender_index = clients.index(sender)
-        sender_nickname = nicknames[sender_index]
-        try:
-            recipient.send(f'[귓속말] {sender_nickname}> {message}'.encode('utf-8'))
-        except:
-            remove_client(recipient)
+def send_private_message(sender, target_nickname, message):
+    if target_nickname in nicknames:
+        sender_nick = nicknames[clients.index(sender)]
+        target_index = nicknames.index(target_nickname)
+        target_client = clients[target_index]
+        private_msg = f'[귓속말] {sender_nick}> {message}'
+        target_client.send(private_msg.encode('utf-8'))
     else:
-        sender.send(f'사용자 {to_nickname}를 찾을 수 없습니다.'.encode('utf-8'))
+        sender.send(f'{target_nickname}님을 찾을 수 없습니다.'.encode('utf-8'))
 
 def remove_client(client):
     if client in clients:
@@ -63,32 +62,38 @@ def remove_client(client):
         clients.remove(client)
         nicknames.remove(nickname)
 
-def receive():
+def accept_connections(server_socket):
     while True:
-        client, address = server.accept()
-        print(f'연결됨: {str(address)}')
-
+        client, _ = server_socket.accept()
         client.send('닉네임을 입력하세요: '.encode('utf-8'))
         nickname = client.recv(1024).decode('utf-8')
-        nicknames.append(nickname)
-        clients.append(client)
 
-        print(f'닉네임: {nickname}')
-        welcome_message = f'{nickname}님이 입장하셨습니다.'
-        broadcast(welcome_message)
-        client.send('서버에 접속되었습니다! 채팅을 시작하세요.'.encode('utf-8'))
+        clients.append(client)
+        nicknames.append(nickname)
+
+        welcome = f'{nickname}님이 입장하셨습니다.'
+        broadcast(welcome)
+        client.send('서버에 연결되었습니다. 채팅을 시작하세요.'.encode('utf-8'))
 
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
 
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('소캣 생성완료')
+
+    host = socket.gethostname()
+    host_ip = socket.gethostbyname(host)
+    port = 999
+
+    print(f'호스트 이름: {host}')
+    print(f'호스트 IP: {host_ip}')
+
+    server_socket.bind((host_ip, port))
+    server_socket.listen(3)
+    print(f'서버가 {port}번 포트에서 실행 중입니다...')
+
+    accept_connections(server_socket)
 
 if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = 55555
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen()
-
-    print(f'서버가 {host}:{port} 에서 실행 중입니다...')
-    receive()
+    start_server()
